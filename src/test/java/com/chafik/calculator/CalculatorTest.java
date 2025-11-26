1b9b1bb923c0355ea8498b534354555c15ff3e33
Chafik-Aymane <helloaymane@gmail.com>
2025-11-26 11:29:58 +0100

    ajout de ParametrizedTest

--- src/test/java/com/chafik/calculator/CalculatorTest.java
+++ src/test/java/com/chafik/calculator/CalculatorTest.java
@@ -1,5 +1,8 @@
 package com.chafik.calculator;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -16,18 +19,23 @@ public class CalculatorTest {
         //calc = null;
     }
 
-    @Test
-    void add_devrait_calculer_la_somme_de_deux_int(){
-        int resultat = 15;
-        int actuel = calc.add(9,6);
-
-        assertEquals(resultat, actuel, "6+9 devrait etre egal a 15");
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    void add_devrait_calculer_la_somme_de_deux_int(int opG,int opD, int resultatAttendu){
+        int actuel = Calculator.add(opG,opD);
+        assertEquals(resultatAttendu, actuel,
+                String.format("L'addition de %d + %d devrait etre %d",opG,opD,resultatAttendu));
     }
-
-    @Test
+    
     void divide_devrait_calculer_la_division_d_int_par_int(){
         int resultat = 5;
-        int actuel = calc.divide(10,2);
+        int actuel = Calculator.divide(10,2);
 
         assertEquals(resultat, actuel, "10/2 devrait etre egal a 5");
     }

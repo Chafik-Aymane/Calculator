98c44f3dcb7c3e0f8f9e60d84ae132e56c322ba9
Chafik-Aymane <helloaymane@gmail.com>
2025-11-26 10:46:53 +0100

    nouvelle fonction : add et divide

--- src/main/java/com/chafik/calculator/Calculator.java
+++ src/main/java/com/chafik/calculator/Calculator.java
@@ -1,5 +1,10 @@
 package com.chafik.calculator;
 
 public class Calculator {
-
+    public int add(int opG,int opD){
+        return opD+opG;
+    }
+    public int divide(int opG,int opD){
+        return opG/opD;
+    }
 }
--- src/test/java/com/chafik/calculator/CalculatorTest.java
+++ src/test/java/com/chafik/calculator/CalculatorTest.java
@@ -1,6 +1,24 @@
 package com.chafik.calculator;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CalculatorTest {
+    private final Calculator calc = new Calculator();
+
+    @Test
+    void add_devrait_calculer_la_somme_de_deux_int(){
+        int resultat = 15;
+        int actuel = calc.add(9,6);
+
+        assertEquals(resultat, actuel, "6+9 devrait etre egal a 15");
+    }
+
+    @Test
+    void divide_devrait_calculer_la_division_d_int_par_int(){
+        int resultat = 5;
+        int actuel = calc.divide(10,2);
+
+        assertEquals(resultat, actuel, "10/2 devrait etre egal a 5");
+    }
 }
